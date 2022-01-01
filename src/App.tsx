0678b04742b81db74dc23dbf4b58913566607729
Антон Королев <gimtonic@rambler.ru>
2022-01-02 01:52:01 +0700

    chore: add change background color

--- src/App.tsx
+++ src/App.tsx
@@ -35,21 +35,44 @@ const App = () => {
           );
         }
       );
+
+    // Initialize button with users' preferred color
+    let changeColor = document.getElementById("changeColor");
+
+    chrome.storage.sync.get("color", ({ color }) => {
+      if (changeColor) {
+        changeColor.style.backgroundColor = color;
+      }
+    });
+
+    // When the button is clicked, inject setPageBackgroundColor into current page
+    changeColor?.addEventListener("click", async () => {
+      let [tab] = await chrome.tabs.query({
+        active: true,
+        currentWindow: true,
+      });
+
+      chrome.scripting.executeScript({
+        target: { tabId: Number(tab.id) },
+        func: setPageBackgroundColor,
+      });
+    });
+
+    // The body of this function will be executed as a content script inside the
+    // current page
+    function setPageBackgroundColor() {
+      chrome.storage.sync.get("color", ({ color }) => {
+        document.body.style.backgroundColor = color;
+      });
+    }
   });
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Title</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {title}
-        </a>
+        <button id="changeColor">Change color</button>
+        {title}
       </header>
     </div>
   );
